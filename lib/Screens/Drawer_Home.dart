196a45f31060be7587b95615319ea8a83624b406
Enas <142669558+EnasSAbuMousa@users.noreply.github.com>
2024-07-18 11:50:46 +0300

    Update Drawer_Home.dart

--- lib/Screens/Drawer_Home.dart
+++ lib/Screens/Drawer_Home.dart
@@ -37,7 +37,7 @@ class _DrawerHomeState extends State<DrawerHome> {
               padding: const EdgeInsets.only(left: 10.0 ,right: 10.0),
               child: Container(
                   decoration: BoxDecoration(color: const Color(0xBF706E6E),borderRadius: BorderRadius.circular(10)),
-                  height: 56,
+                  height: 55,
                   child: ListTile(
                     leading: const Icon(Icons.home,color: Colors.white),
                     title: const Text('Home',style: TextStyle(color: Color(0xFFFFFFFF)),),
